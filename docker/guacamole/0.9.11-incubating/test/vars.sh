6e1b4000df340aff571ea43390ca1748eb38e5ac
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-04-20 15:17:04 -0400

    Initial revision

---
+++ docker/guacamole/0.9.11-incubating/test/vars.sh
@@ -0,0 +1,11 @@
+export GUACD_IMAGE_NAME=${GUACD_IMAGE_NAME:-"docker.io/glyptodon/guacd:0.9.11-incubating"}
+export GUACAMOLE_IMAGE_NAME=${GUACAMOLE_IMAGE_NAME:-"docker.io/glyptodon/guacamole:0.9.11-incubating"}
+export MYSQL_IMAGE_NAME=${MYSQL_IMAGE_NAME:-"docker.io/glyptodon/mysql:0.9.11-incubating"}
+
+export GUACD_CONTAINER_NAME=${GUACD_CONTAINER_NAME:-"some-guacd"}
+export GUACAMOLE_CONTAINER_NAME=${GUACAMOLE_CONTAINER_NAME:-"some-guacamole"}
+export MYSQL_CONTAINER_NAME=${MYSQL_CONTAINER_NAME:-"some-mysql"}
+
+export GUACAMOLE_DATABASE=${GUACAMOLE_DATABASE:-"mydb1"}
+export GUACAMOLE_USER=${GUACAMOLE_USER:-"user1"}
+export GUACAMOLE_PASSWORD=${GUACAMOLE_PASSWORD:-"pass1"}
